531c5815431f0cb6bf2794de5aba426c26b6009a
Adrien Delacroix <57764513+Delacroix-75020@users.noreply.github.com>
2023-07-19 15:42:54 +0200

    Ajout proto modele

--- API/solaris_app/models.py
+++ API/solaris_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TypeVaisseau(models.Model):
+    nom = models.CharField(max_length=100)
+
+class TypePlanete(models.Model):
+    nom = models.CharField(max_length=100)
+
+class Planete(models.Model):
+    nom = models.CharField(max_length=100)
+    type_planete = models.ForeignKey(TypePlanete, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+
+class ActivitePlanete(models.Model):
+    planete = models.ForeignKey(Planete, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Vol(models.Model):
+    planete_depart = models.ForeignKey(Planete, related_name='vols_depart', on_delete=models.CASCADE)
+    planete_arrivee = models.ForeignKey(Planete, related_name='vols_arrivee', on_delete=models.CASCADE)
+    type_vaisseau = models.ForeignKey(TypeVaisseau, on_delete=models.CASCADE)
+    date_vol = models.DateField()
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vol = models.ForeignKey(Vol, on_delete=models.CASCADE)
+    date_reservation = models.DateField()
+
+class Hotel(models.Model):
+    planete = models.ForeignKey(Planete, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=200)
