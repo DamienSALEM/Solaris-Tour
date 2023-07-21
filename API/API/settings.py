83bf380e2af218d9b80a8a68cfdcc044ba8c551e
Damien SALEM <d.salem@ecole-ipssi.net>
2023-07-21 12:45:26 +0200

    migrations

--- API/API/settings.py
+++ API/API/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'solaris_app',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK = {
